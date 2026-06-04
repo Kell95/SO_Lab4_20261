b82526f83adfe7385e5cad7cbd9570cf81cc1ce7
brayan <b.jaraba@udea.edu.co>
2026-06-04 15:56:12 -0500

    adding fibonacci sultions

---
+++ fibonacci/fibonacci.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct {
+  int n;
+  long long  *arr;
+} fibonacci_args;
+
+void *calculate_fibonacci(void *args) {
+  fibonacci_args *fargs = (fibonacci_args *)args;
+  int n = fargs->n;
+  long long  *arr = fargs->arr;
+
+  arr[0] = 1;
+  arr[1] = 2;
+
+  for (int i = 2; i < n; i ++ ) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  assert(argc == 2 && "usage: fibonacci <n>");
+  pthread_t tread_id;
+
+  int n = atoi(argv[1]);
+  long long  *arr = malloc(sizeof(long long) * n);
+  assert(arr != NULL);
+  fibonacci_args fargs = {n, arr};
+
+  pthread_create(&tread_id, NULL, calculate_fibonacci, &fargs);
+  pthread_join(tread_id, NULL);
+
+  for (int i = 0; i < n; i ++ ) {
+    printf("%lld ", arr[i]);
+  }
+  printf("\n");
+  free(arr);
+}
